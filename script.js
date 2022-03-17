150766622d246886f395be094df3a9b837e0ab97
chidiebereojingwa <ojingwaoji@gmail.com>
2022-03-17 22:24:02 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
-console.log("Governor")
-import data from './data.js';
 
-// Open -close mobile menu
+const data = [
+  {
+    name: 'Chidiebere Ojingwa',
+    title: 'President GHUB community',
+    description:
+      'Chidiebere is the president and poinier of a fast growing ICT community GHUB',
+    imageUrl: './images/speaker1.png',
+  },
+  {
+    name: 'Favour Precouse',
+    title: 'President of African Child Care',
+    description:
+      'Nigerian-American economist, fair trade leader, environmental sustainability advocate, human welfare champion, sustainable finance maven and global development expert. Since March 2021',
+    imageUrl: './images/speaker2.jpg',
+  },
+  {
+    name: 'Jonah Jang',
+    title:
+      'Former Senator of the Federal Republic of Nigeria',
+    description:
+      'Air Commodore Jonah David Jang CON is a Nigerian statesman who was the Governor of Plateau State from 2007 to 2015',
+    imageUrl: './images/Jonah-Jang.jpg',
+  },
+  {
+    name: 'Clinton Garuba',
+    title:
+      'Managing Director · Jay FM 10.9 Jos',
+    description:
+      'Clinton Garuba - Managing Director - Jay FM 10.9 Jos.',
+    imageUrl: './images/clinton.jpg',
+  },
+  {
+    name: 'Lila Tretikov',
+    title:
+      'Executive Director of the wikimedia Foundation',
+    description:
+      'Lila Tretikov is the Executive Director of the wikimedia Foundation, the nonprofit, organization that operates wikipedia. Wikipedia is freely available in 290 languages.',
+    imageUrl: './images/random.jpg',
+  },
+  {
+    name: 'Chisom Ejindu',
+    title:
+      'CEO of Creative Skin Care and Digital Marketing Community',
+    description:
+      'Chisom had been leading open-source projects at the Somie Foundation such as the open source movement. ',
+    imageUrl: './images/chisom.jpg',
+  },
+];
+
+
 // Menu button
 const menuBtn = document.querySelector('.menu-btn');
 const menuNavEle = document.querySelector('nav');
@@ -51,12 +98,12 @@ if (toggleMore) {
   });
 }
 
-// Create speaker cards from data.js
+// Creating speaker cards from data.js
 const firstSpeakersEle = document.querySelector('.featured-speakers');
 const restSpeakersEle = document.querySelector('.important-speakers');
 let speakerCounter = 0;
 
-const insertCard = (element, data) => {
+function insertCard(element, data){
   const {
     name, title, description, imageUrl,
   } = data;
